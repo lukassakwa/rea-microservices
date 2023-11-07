51de8207a7f37fe5dbcda927194fecc89b9adaa1
oliwier <oliviersakwa@gmail.com>
2023-11-07 23:43:09 +0100

    init temp microservices

---
+++ rea-user-offer-service/src/main/java/com/rea/system/useroffer/UserOfferApplication.java
@@ -0,0 +1,13 @@
+package com.rea.system.useroffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOfferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserOfferApplication.class, args);
+    }
+
+}
